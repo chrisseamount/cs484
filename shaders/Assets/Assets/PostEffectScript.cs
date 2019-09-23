7a0c382c99af0ef6acaeb13b3420e14e333130fa
Chris <chris_seamount@hotmail.com>
2019-09-22 23:53:43 -0800

    adding hw3

---
+++ shaders/Assets/Assets/PostEffectScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostEffectScript : MonoBehaviour {
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Color[] pixels = new Color[1920 * 1080];
+
+        for (int x = 0; x < 1920; x++)
+        {
+            for (int y = 0; y <1080; y++)
+            {
+                pixels[x + y * 1080].r = 1.18f * 2.17f;
+            }
+        }
+        Graphics.Blit(source, destination);
+    }
+
+    
+}
---
+++ shopthemegame/Assets/movment.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movment : MonoBehaviour {
+
+    public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0.0f);
+
+        animator.SetFloat("horizontal", movement.x);
+        animator.SetFloat("vertical", movement.y);
+
+        
+        transform.position = transform.position + movement * Time.deltaTime;
+       
+    }
+}
